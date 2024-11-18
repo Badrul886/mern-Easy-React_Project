ab4754bf3bacf0ffeb384a9b825f0822cb7a00ba
Badrul886 <turjame02@gmail.com>
2024-11-18 21:02:02 +0600

    First project completed in windows

--- backend/server.js
+++ backend/server.js
@@ -17,21 +17,12 @@ app.use(express.json()); //allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-// the if block is used for production and deployment
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
 // Serve Vite frontend build files
 if (process.env.NODE_ENV === "production") {
-  // Serve the frontend build files
-  const staticPath = path.join(__dirname, "frontend", "dist");
-  app.use(express.static(staticPath));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(staticPath, "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
